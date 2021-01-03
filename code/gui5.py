e6d9af8df09195f5722b339195579cb710d0ccc1
BryanGonz <bgon@ucla.edu>
2021-01-02 19:25:39 -0800

    Github code review corrections

--- code/gui5.py
+++ code/gui5.py
@@ -1,15 +1,36 @@
-# Running into errors with the do_clustering() funtion
-
-from clusterer import do_clustering
-from VectorModelBuilder import VectorModelBuilder
 import tkinter as tk
+from clusterer import do_clustering
 from tkinter.filedialog import askopenfilename, askdirectory
+from VectorModelBuilder import VectorModelBuilder
 
-VMB_ARGS = (("Dataset", ""),("Count Method", "ngram"),("Weighting", "ppmi"),("Output Directory","../vector_data/"),("Output File Name","None"),("Value of N", "3"))
-CLUSTERER_ARGS = (("Stem of Files",""),("Output File","../vector_data/"),("V Scalar","1"),("Constrain Partition","False"),("Constrain PCS","False"))
+VMB_ARGS = (
+    ("Dataset", ""), 
+    ("Count Method", "ngram"), 
+    ("Weighting", "ppmi"), 
+    ("Output Directory", "../vector_data/"), 
+    ("Output File Name", "None"), 
+    ("Value of N", "3")
+)
+
+CLUSTERER_ARGS = (
+    ("Stem of Files", ""), 
+    ("Output File", "../vector_data/"), 
+    ("V Scalar", "1"), 
+    ("Constrain Partition", "False"), 
+    ("Constrain PCS", "False")
+)
 
 LABELS = ["  Run  ","Browse...","False","True"]
-FILE_TYPE = (("Text Files", "*.txt"),("All Files", "*.*"))
+
+BOOL_LABELS = [True, False]
+
+FILE_TYPE = (
+    ("Text Files", "*.txt"),
+    ("All Files", "*.*")
+)
+
+COUNT_METHODS = ["ngram"]
+
 WEIGHT_METHODS = ["ppmi", "probability", "conditional_probability", "pmi", "none"]
 
 # Two classes to help initiate the algorithm
@@ -33,12 +54,12 @@ class Clusterer_tk():
 
     def run(self):
         self.v_scalar = int(self.v_scalar)
-        if (self.constrain_partition == "True"):
+        if self.constrain_partition == "True":
             self.constrain_partition = True
         else:
             self.constrain_partition = False
 
-        if (self.constrain_pcs == "True"):
+        if self.constrain_pcs == "True":
             self.constrain_pcs = True
         else:
             self.constrain_pcs = False
@@ -57,11 +78,9 @@ vmb_frame.grid(row=0,column=0,padx=10,pady=10,sticky="nswe")
 vector_model_builder = tk.Label(master=vmb_frame,text="Vector Model Builder")
 vector_model_builder.grid(row=0,column=0,sticky="w")
 # looping to populate labels
-j=1
 for i, text in enumerate(VMB_ARGS):
     label = tk.Label(master=vmb_frame,text=VMB_ARGS[i][0])
-    label.grid(row=j,column=0,sticky="e")
-    j+=1
+    label.grid(row=i+1,column=0,sticky="e")
 
 # Argument 1 Dataset
 def open_arg1_browse():
@@ -73,6 +92,7 @@ def open_arg1_browse():
         return
     arg_path_ent.delete("0", tk.END)
     arg_path_ent.insert(tk.END, filepath)
+
 arg_path_ent = tk.Entry(master=vmb_frame,width=40)
 arg_path_ent.grid(row=1,column=1,sticky="w")
 arg_path_ent.insert(0,VMB_ARGS[0][1])
@@ -86,7 +106,6 @@ method_ent.insert(0,VMB_ARGS[1][1])
 
 # Argument 3 Weighting arg
 vmb_weight_var = tk.StringVar(vmb_frame)
-    # add additional options for weighting here
 vmb_weight_var.set(VMB_ARGS[2][1])
 vmb_weight_menu = tk.OptionMenu(vmb_frame, vmb_weight_var,*WEIGHT_METHODS)
 vmb_weight_menu.grid(row=3,column=1,sticky="w")
@@ -99,6 +118,7 @@ def open_outdir_browse():
         return
     outdir_ent.delete("0", tk.END)
     outdir_ent.insert(tk.END, dirpath)
+
 outdir_ent = tk.Entry(master=vmb_frame,width=40)
 outdir_ent.grid(row=4,column=1,sticky="w")
 outdir_ent.insert(0,VMB_ARGS[3][1])
@@ -120,15 +140,16 @@ def run_vector_model_builder():
     # Check for the default case of None for output file name
     outfile_arg = outf_name_ent.get()
     n_val = n_ent.get()
-    if (outfile_arg == "None"):
+    if outfile_arg == "None":
         outfile_arg = None
     # Convert value of n from string to int or set the default to 3 if blank
-    if (n_val == ""):
+    if n_val == "":
         n_val = 3
     else:
         n_val = int(n_val)
     Vector_tk = VectorModelBuilder_tk(arg_path_ent.get(),method_ent.get(),vmb_weight_var.get(),outdir_ent.get(),outfile_arg,n_val)
     Vector_tk.run()
+
 run_VectorModelBuilder = tk.Button(master=vmb_frame,command=run_vector_model_builder,text=LABELS[0])
 run_VectorModelBuilder.grid(row=7,column=0, sticky="w")
 #########################################################################
@@ -139,11 +160,9 @@ clusterer_frame.grid(row=1,column=0,padx=10,pady=10,sticky="nswe")
 clusterer = tk.Label(master=clusterer_frame,text="Clusterer")
 clusterer.grid(row=0,column=0,sticky="w")
 # Looping to populate the labels
-j=1
 for i, text in enumerate(CLUSTERER_ARGS):
     label = tk.Label(master=clusterer_frame,text=CLUSTERER_ARGS[i][0])
-    label.grid(row=j,column=0,sticky="e")
-    j+=1
+    label.grid(row=i+1,column=0,sticky="e")
 
 # Argument 1
 file_name_ent = tk.Entry(master=clusterer_frame,width=40)
@@ -158,6 +177,7 @@ def output_file_browse():
         return
     output_file_ent.delete("0", tk.END)
     output_file_ent.insert(tk.END, dirpath)
+
 output_file_ent = tk.Entry(master=clusterer_frame,width=40)
 output_file_ent.grid(row=2,column=1,sticky="w")
 output_file_ent.insert(0,CLUSTERER_ARGS[1][1])
@@ -177,15 +197,18 @@ constrain_partition_menu.grid(row=4,column=1,sticky="w")
 
 # Argument 5
 constrain_pcs_var = tk.StringVar(clusterer_frame)
-    # add additional options for weighting here
 constrain_pcs_var.set(CLUSTERER_ARGS[4][1])
 constrain_pcs_menu  = tk.OptionMenu(clusterer_frame, constrain_pcs_var,*LABELS[2:])
 constrain_pcs_menu.grid(row=5,column=1,sticky="w")
 
 # To run, the first two fields must be filled
 def run_clusterer():
-    do_clustering_tk = Clusterer_tk(file_name_ent.get(),output_file_ent.get(),v_scalar_ent.get(),constrain_partition_var.get(),constrain_pcs_var.get())
+    do_clustering_tk = Clusterer_tk(
+        file_name_ent.get(), output_file_ent.get(), v_scalar_ent.get(), 
+        constrain_partition_var.get(), constrain_pcs_var.get()
+    )
     do_clustering_tk.run()
+
 run_clusterer_btn = tk.Button(master=clusterer_frame,command=run_clusterer,text=LABELS[0])
 run_clusterer_btn.grid(row=6,column=0, sticky="w")
 ########################################################################
@@ -193,12 +216,14 @@ run_clusterer_btn.grid(row=6,column=0, sticky="w")
 def run_both():
     run_vector_model_builder()
     name = outf_name_ent.get()
-    if (name == "None") or (name == ""):
+    if not name:
         ''' Print an Error '''
         print("Name of file is None\nCan not do clustering")
         return
     cluster = Clusterer_tk(name,outf_name_ent.get(),v_scalar_ent.get(),constrain_partition_var.get(),constrain_pcs_var.get())
     cluster.run()
+
 run_all_btn = tk.Button(master=window,text="  Run Both  ",command=run_both)
 run_all_btn.grid(row=2,column=0,pady=10)
+
 window.mainloop()
